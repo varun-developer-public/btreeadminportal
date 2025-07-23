5fe9ee32576b548792ddad3925fdc10ca215238c
varun-developer-public <varundev.bytes@gmail.com>
2025-07-23 10:58:32 +0530

    `Updated settings, templates, and static files for BTRS Portal`

--- core/settings.py
+++ core/settings.py
@@ -14,8 +14,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-g7ccbck23uiam(
 
 # --- PRODUCTION SETTINGS ---
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['btrees.in', 'admin.btrees.in']
+# DEBUG = False
+# ALLOWED_HOSTS = ['btrees.in', 'admin.btrees.in']
 
 
 # Application definition
@@ -70,16 +70,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database - Production (PostgreSQL)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btree_db',
-        'USER': 'btree_user',
-        'PASSWORD': 'BTree@2025',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'btree_db',
+#         'USER': 'btree_user',
+#         'PASSWORD': 'BTree@2025',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
@@ -104,12 +104,12 @@ STATIC_URL = 'static/'
 
 # The absolute path to the directory where `collectstatic` will collect static files for deployment.
 # This directory is the destination for all static files.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# A list of directories where Django will look for your project's source static files.
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# # A list of directories where Django will look for your project's source static files.
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 
 # --- MEDIA FILES CONFIGURATION (User-uploaded content) ---
@@ -124,16 +124,16 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 
-# DEBUG = True
-# ALLOWED_HOSTS = ['*']
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
-# # developement db
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+# developement db
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
