9a75e246763a4c0e72e5e6d6d043137d7cb3bae6
varun-developer-public <varundev.bytes@gmail.com>
2025-10-08 13:34:44 +0530

    `Added new features and styles to student report page, including placement details, interview details, and payment details, and made various layout and design changes.`

---
+++ paymentdb/templatetags/payment_tags.py
@@ -0,0 +1,4 @@
+from django import template
+from decimal import Decimal
+
+register = template.Library()
---
+++ studentsdb/templatetags/student_tags.py
@@ -0,0 +1,21 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_attribute(obj, attr):
+    return getattr(obj, attr, "")
+
+
+@register.filter
+def get_emi_pending_amount(payment, emi_number):
+    """
+    Calculates the pending amount for a specific EMI.
+    """
+    try:
+        emi_amount = getattr(payment, f'emi_{emi_number}_amount') or Decimal('0')
+        emi_paid_amount = getattr(payment, f'emi_{emi_number}_paid_amount') or Decimal('0')
+        pending_amount = emi_amount - emi_paid_amount
+        return pending_amount if pending_amount > 0 else Decimal('0')
+    except (AttributeError, TypeError):
+        return Decimal('0')
