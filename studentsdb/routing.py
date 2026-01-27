eda7f8907a4e4b4336e6cd9b1c4a74d1fba9a7b5
varun-developer-public <varundev.bytes@gmail.com>
2026-01-27 12:26:08 +0530

    `Update routing.py to include trailing slash in URL patterns`

--- studentsdb/routing.py
+++ studentsdb/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from .consumers import StudentConsumer, StudentRemarksConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/student/(?P<student_id>\d+)/$', StudentConsumer.as_asgi()),
-    re_path(r'ws/student/remarks/$', StudentRemarksConsumer.as_asgi()),
+    re_path(r'^ws/student/(?P<student_id>\d+)/$', StudentConsumer.as_asgi()),
+    re_path(r'^ws/student/remarks/?$', StudentRemarksConsumer.as_asgi()),
 ]
